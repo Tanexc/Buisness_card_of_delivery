fb5761ad0abfd912fc3b68ac1b29d33fb6858b9a
Arthur231456 <artur.231456@gmail.com>
2022-08-22 21:40:38 +0500

    main.css | add other text to contacts

--- main.py
+++ main.py
@@ -10,4 +10,5 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run()
+    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
