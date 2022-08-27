b3e00a379521105d9ceb4ec12e11fe113ac46f9a
Arthur231456 <artur.231456@gmail.com>
2022-08-27 19:56:56 +0500

    bugs fix

--- main.py
+++ main.py
@@ -10,5 +10,4 @@ def main():
 
 
 if __name__ == '__main__':
-    app.run()
-    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
